6546ea2320cc4ea8f3ddaa675aa9bc75fe0ae68b
Dongximing <dongx34@myumanitoba.ca>
2024-03-22 14:47:36 -0400

    add the process

--- tutorials/test.py
+++ tutorials/test.py
@@ -13,21 +13,43 @@ from captum.attr import (
     TextTemplateInput,
     ProductBaselines,
 )
-eval_prompt = "I love you and"
-model = AutoModelForCausalLM.from_pretrained('gpt2')
-tokenizer = AutoTokenizer.from_pretrained('gpt2')
+def load_model(model_name, bnb_config):
+    n_gpus = torch.cuda.device_count()
+    max_memory = "10000MB"
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        quantization_config=bnb_config,
+        device_map="auto", # dispatch efficiently the model on the available ressources
+        max_memory = {i: max_memory for i in range(n_gpus)},
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=True)
+
+    # Needed for LLaMA tokenizer
+    tokenizer.pad_token = tokenizer.eos_token
+
+    return model, tokenizer
+
+def create_bnb_config():
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16,
+    )
+
+    return bnb_config
+eval_prompt = """the sentiment of 'I am happy' is positive or negative: the answer is"""
+model = AutoModelForCausalLM.from_pretrained('meta-llama/Llama-2-13b-chat-hf')
+tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-2-13b-chat-hf')
 
 model_input = tokenizer(eval_prompt, return_tensors="pt")
 print(model_input["input_ids"])
-# model.eval()
-# with torch.no_grad():
-#     output_ids = model.generate(model_input["input_ids"], max_new_tokens=15)[0]
-#     response = tokenizer.decode(output_ids, skip_special_tokens=True)
-#     print(response)
-fa = FeatureAblation(model)
+
+# fa = FeatureAblation(model)
 
 ig = LayerIntegratedGradients()
-llm_attr = LLMAttribution(fa, tokenizer)
+llm_attr = LLMAttribution(ig, tokenizer)
 
 inp = TextTokenInput(
     eval_prompt,
